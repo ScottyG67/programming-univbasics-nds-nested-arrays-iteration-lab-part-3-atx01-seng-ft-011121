e2b4996e03efd3bb41e97106043aa7ce0ad59daf
Scott Gloyna <scott.gloyna01@gmail.com>
2020-12-23 01:44:53 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,5 +7,11 @@ first_index =0
 while first_index < src.count do
   second_index = 0 
   while second_index < src[first_index].count do
-    if src[first_index][second_index].is_a
+    if src[first_index][second_index].is_a? String
+      string_array << src[first_index][second_index]
+    end
+    second_index += 1 
+  end
+  first_index +=1 
+string_array
 end
