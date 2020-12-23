ca9471b846899c17c1229465a1c86059d633411f
Scott Gloyna <scott.gloyna01@gmail.com>
2020-12-23 02:56:01 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -2,13 +2,7 @@ def join_nested_strings(src)
   # src will be an Array of Arrays of Strings and Integers
   # Combine all Strings present in the AoA into a single value and return it
 
-src = [
-  ["The", 4, "quick"],
-  [-1, "brown", "fox", 30],
-  ["studied", 101, 233, "Ruby"]
-]
-
-  require 'pry'
+require 'pry'
 message =""
 first_index =0 
 while first_index < src.count do
